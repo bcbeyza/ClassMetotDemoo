282b7a779fa249b44f563d87f289e9270cc2af52
Beyza Nur Çetiner <190101036@ogrenci.yalova.edu.tr>
2021-02-07 15:29:40 +0300

    Add project files.

---
+++ ClassMetotDemoo/MusteriManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemoo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+
+            Console.WriteLine("{0} Id nolu musteri eklendi", musteri.Id);
+
+        }
+        public void Listele(Musteri[] musteriler)
+        {
+            for (int i = 0; i < musteriler.Length; i++)
+            {
+                Console.WriteLine("Musteri Id:" + musteriler[i].Id);
+                Console.WriteLine("Musteri Adi: " + musteriler[i].Adi);
+                Console.WriteLine("Musteri Soyadi: " + musteriler[i].Soyadi);
+                Console.WriteLine("Musteri Meslegi: " + musteriler[i].Meslek);
+                Console.WriteLine("Musteri Yasi: " + musteriler[i].Yasi);
+                Console.WriteLine("  ");
+            }
+
+
+
+
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("{0} Id nosuna sahip musteri silinmistir.", musteri.Id);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ ClassMetotDemoo/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ClassMetotDemoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 333333333;
+            musteri1.Adi = "Mavi";
+            musteri1.Soyadi = "D* ";
+            musteri1.Meslek = "Muhendis";
+            musteri1.Yasi = 44;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 1111111;
+            musteri2.Adi = "Ayse";
+            musteri2.Soyadi = "B* ";
+            musteri2.Meslek = "Sporcu";
+            musteri2.Yasi = 33;
+
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id);
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.Soyadi);
+                Console.WriteLine(musteri.Meslek);
+                Console.WriteLine(musteri.Yasi);
+                Console.WriteLine(" ");
+
+
+
+            }
+
+
+
+            Console.WriteLine("------------Metot ile Yapim------------------");
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+
+            musteriManager.Ekle(musteri2);
+
+            Console.WriteLine("*****************************************");
+
+            musteriManager.Listele(musteriler);
+
+            Console.WriteLine("*****************************************");
+
+            musteriManager.Sil(musteri1);
+
+            musteriManager.Sil(musteri2);
+
+            Console.WriteLine("********************************************");
+
+        }
+    }
+}
